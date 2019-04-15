c60c2701bc875822a284215ef03b913b93bfc49e
jsurt <jacksurtees@gmail.com>
2019-04-14 23:22:05 -0400

    another commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-//exports.CLIENT_ORIGIN = "https://intense-headland-64831.herokuapp.com";
-exports.CLIENT_ORIGIN = "http://localhost:3000";
+exports.CLIENT_ORIGIN = "https://intense-headland-64831.herokuapp.com";
+//exports.CLIENT_ORIGIN = "http://localhost:3000";
 exports.PORT = process.env.PORT || 8080;
 exports.DATABASE_URL =
   process.env.DATABASE_URL || "mongodb://localhost/matcher-dev-db";
